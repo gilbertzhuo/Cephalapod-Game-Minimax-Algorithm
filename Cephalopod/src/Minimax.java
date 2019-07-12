1b6ffc08a33810e012db07797e3b68ce77dfe331
Gilbert Zhuo <gilbertzhuo@gmail.com>
2019-07-12 10:42:02 +1000

    Bug fix

--- Cephalopod/src/Minimax.java
+++ Cephalopod/src/Minimax.java
@@ -13,22 +13,18 @@ public class Minimax {
 	    for (int i = 0; i<3; i++) 
 	    { 
 	        for (int j = 0; j<3; j++) 
-	        { 
-	            // Check if cell is empty 
+	        {  
+	        	
 	            if (board.grid[i][j]==0) 
 	            { 
 	            	Board temp = new Board(board);
-	                // Make the move 
+	               
 	                temp.BoardMove(player, i+1, j+1);
 	                temp.capture(player, i+1, j+1);
 	                
-	                // compute evaluation function for this 
-	                // move. 
+	            
 	                int moveVal = miniMax(player,temp); 
 	  
-	                // If the value of the current move is 
-	                // more than the best value, then update 
-	                // best/ 
 	                if (moveVal > bestVal) 
 	                { 
 	                    bestMove.row = i; 
@@ -44,7 +40,7 @@ public class Minimax {
 	} 
 	public int miniMax(int player, Board board) {
 		
-		//Check for empty
+		
 		if(board.checkEmpty()==false) {
 			int total = 0;
 			  for(int i =0; 3>i;i++){
@@ -52,7 +48,7 @@ public class Minimax {
 			               total += board.grid[i][j];
 			        }
 			    }
-			//Getting the score
+			  
 			if(total ==0) {
 				return 0;
 			}else if(total >1) {
@@ -66,11 +62,10 @@ public class Minimax {
 	        { 
 	            for (int j = 0; j<3; j++) 
 	            { 
-	                // Check if cell is empty 
+	            
 	                if (board.grid[i][j]==0) 
 	                { 
-	                    // Make the move 
-	                
+	             
 	                    board.BoardMove(player, i+1, j+1);
 	                    board.capture(player, i+1, j+1);
 	                    best = Math.max( best, 
@@ -87,7 +82,7 @@ public class Minimax {
 	        { 
 	            for (int j = 0; j<3; j++) 
 	            { 
-	                // Check if cell is empty 
+	              
 	                if (board.grid[i][j]==0) 
 	                { 
 	                    
